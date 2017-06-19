8d49c7e685586c88c5d9034153c2aaceb4f7587b
owlg <chewjianyue2017@gmail.com>
2017-06-19 12:28:12 +0800

    Colors on Server Log integration
    Colors with status colors, etc changes to index.

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,10 @@
 var http = require('http');
+var colors = require('colors');
+
 http.createServer(function (req, res) {
 
 
 }).listen(80, 127.0.0.1);
 
 console.log('Server is running');
+console.log('Good'.green);
